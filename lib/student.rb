c0e99b2f351dbcbc25393f54bcca229059a74ae0
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-20 12:23:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -104,9 +104,8 @@ class Student
     LIMIT 1
     SQL
     
-      DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-      end
+    first_student_row = DB[:conn].execute(sql)[0]
+     self.new_from_db(first_student_row)
     end
   
    def self.all_students_in_grade_X(x)
